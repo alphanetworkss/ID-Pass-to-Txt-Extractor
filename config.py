e24bd36a5fba86dac323396d4d06484fd316a530
Team AphaNetwork <160410546+alphanetworkss@users.noreply.github.com>
2025-01-15 21:23:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =27107197
+api_hash = "3cb8282036ceb31f57cf321270a590d2"
+bot_token = "7038615431:AAFO6JhbGfglf0gW21AChOd7xCKfWhZoDic"
+auth_users = "5860841520"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "5860841520"
 owner_users = [int(num) for num in osowner_users.split(",")]
